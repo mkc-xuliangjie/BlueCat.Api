54a39f5db0b4d4ceab4b460148814a06e015f03c
mkc-xuliangjie <xuliangjie19870208@163.com>
2019-09-29 18:07:50 +0800

    code

---
+++ Src/BlueCat.Api/BuleCat.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,152 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuleCat.Common.Extensions
+{
+    /// <summary>
+    /// <see cref="string"/> 对象的扩展类
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 将字符串转换为 int 类型，若不能转换，会抛出错误。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int ToInt(this string str)
+        {
+            return int.Parse(str);
+        }
+
+        /// <summary>
+        /// 将字符串转换为 int 类型，若不能转换，会返回 null。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int? AsInt(this string str)
+        {
+            if (int.TryParse(str, out int value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 将字符串转换为 int 类型，若不能转换，会返回默认值。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns></returns>
+        public static int AsInt(this string str, int defaultValue)
+        {
+            return AsInt(str) ?? defaultValue;
+        }
+
+        /// <summary>
+        ///  将字符串转换为 DateTime 类型，若不能转换，会抛出错误。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this string str)
+        {
+            return DateTime.Parse(str);
+        }
+
+        /// <summary>
+        /// 将字符串转换为 DateTime 类型，若不能转换，会返回 null。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static DateTime? AsDateTime(this string str)
+        {
+            if (DateTime.TryParse(str, out DateTime value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 将字符串转换为 DateTime 类型，若不能转换，会返回默认值。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns></returns>
+        public static DateTime AsDateTime(this string str, DateTime defaultValue)
+        {
+            return AsDateTime(str) ?? defaultValue;
+        }
+
+        /// <summary>
+        /// 将字符串反序列化为对象
+        /// </summary>
+        /// <typeparam name="T">要反序列化的对象类型</typeparam>
+        /// <param name="str">反序列化的字符串</param>
+        /// <param name="settings">反序列化设置选项</param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string str, JsonSerializerSettings settings = null)
+        {
+            return JsonConvert.DeserializeObject<T>(str, settings ?? new JsonSerializerSettings());
+        }
+
+        /// <summary>
+        /// 将字符串反序列化为对象
+        /// </summary>
+        /// <param name="str">反序列化的字符串</param>
+        /// <param name="objectType">要反序列化的类型</param>
+        /// <param name="settings">反序列化设置选项</param>
+        /// <returns></returns>
+        public static object ToObject(this string str, Type objectType, JsonSerializerSettings settings = null)
+        {
+            return JsonConvert.DeserializeObject(str, objectType, settings ?? new JsonSerializerSettings());
+        }
+
+        /// <summary>
+        /// 将字符串反序列化为指定类型的对象
+        /// </summary>
+        /// <param name="str">String.</param>
+        /// <param name="settings">Json Settings.</param>
+        /// <typeparam name="T">要反序列化的的对象类型</typeparam>
+        public static T AsObject<T>(this string str, JsonSerializerSettings settings = null)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return default(T);
+
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(str, settings ?? new JsonSerializerSettings());
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        /// 将字符串反序列化为指定类型的对象
+        /// </summary>
+        /// <param name="str">String</param>
+        /// <param name="objectType">要反序列化的的对象类型</param>
+        /// <param name="settings">Json Settings.</param>
+        /// <returns></returns>
+        public static object AsObject(this string str, Type objectType, JsonSerializerSettings settings = null)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return null;
+
+            try
+            {
+                return JsonConvert.DeserializeObject(str, objectType, settings ?? new JsonSerializerSettings());
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- Src/BlueCat.Api/BuleCat.Common/Http/Filters/ValidateRequestModelAttribute.cs
+++ Src/BlueCat.Api/BuleCat.Common/Http/Filters/ValidateRequestModelAttribute.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using BuleCat.Common.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Reflection;
 
 namespace BuleCat.Common.Http.Filters
 {
@@ -13,7 +15,107 @@ namespace BuleCat.Common.Http.Filters
 
         public virtual void ValidateCore(ActionExecutingContext context)
         {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.SelectMany(m => m.Value.Errors.Select(e => e.ErrorMessage));
+                var errMsg = $"Invalid arguments. {string.Join("", errors)}";
+                InvalidValidate(errMsg, context);
+                return;
+            }
 
+            // validate the params must be not null, then expose "BIZData" and "Account"
+            string userName = context.HttpContext.User.Identity.IsAuthenticated ? context.HttpContext.User.Identity.Name : null;
+            var parameters = context.ActionDescriptor.Parameters.Where(p => p.ParameterType.IsClass && !p.ParameterType.IsPrimitive && typeof(RequsetModel).IsAssignableFrom(p.ParameterType));
+            foreach (var p in parameters)
+            {
+                if (!context.ActionArguments.ContainsKey(p.Name))
+                {
+                    var errMsg = $"Argument '{p.Name}' must be not null.";
+                    InvalidValidate(errMsg, context);
+                    break;
+                }
+
+                var requestModelObj = context.ActionArguments[p.Name];
+                var requestModel = requestModelObj as RequsetModel;
+                var requestModelType = requestModelObj.GetType().GetTypeInfo();
+                if (requestModelType.BaseType != null)
+                {
+                    var bizProperty = requestModelType.GetProperty("BusinessData");
+                    if (bizProperty != null)
+                    {
+                        var (ok, obj) = ResolveData(requestModel.Data, bizProperty.PropertyType);
+                        if (ok)
+                        {
+                            // 验证 BusinessData 对象
+                            var (valid, err) = ValidateBizData(obj);
+                            if (!valid)
+                            {
+                                InvalidValidate(err, context);
+                                return;
+                            }
+
+                            bizProperty.SetValue(requestModelObj, obj);
+                        }
+                    }
+                }
+
+                requestModel.Account = userName;
+            }
+        }
+
+        /// <summary>
+        /// 解析数据，可重写
+        /// </summary>
+        /// <param name="data">要解析的字符串</param>
+        /// <param name="resolveType">要解析的对象类型</param>
+        /// <returns></returns>
+        public virtual (bool, object) ResolveData(string data, Type resolveType)
+        {
+            return (true, data.AsObject(resolveType));
+        }
+
+        /// <summary>
+        /// 无效的验证处理, 可重写
+        /// </summary>
+        /// <param name="errorMessage">错误消息描述</param>
+        /// <param name="context">Action 执行时的上下文</param>
+        public virtual void InvalidValidate(string errorMessage, ActionExecutingContext context)
+        {
+            context.Result = new OkObjectResult(ResponseModel.Failure("4401", errorMessage));
+        }
+
+        /// <summary>
+        ///  验证 BusinessData 对象
+        /// </summary>
+        /// <param name="bizObj">要验证的业务对象</param>
+        /// <returns></returns>
+        public virtual (bool, string) ValidateBizData(object bizObj)
+        {
+            var bizType = bizObj.GetType();
+            if (!ValidatorHub.TryGetValue(bizType, out Type validatorType))
+            {
+                validatorType = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes())
+                                         .FirstOrDefault(t => t.BaseType != null && t.BaseType.IsGenericType
+                                                && t.BaseType.GetGenericTypeDefinition() == typeof(FluentValidation.AbstractValidator<>)
+                                                && t.BaseType.GetGenericArguments()[0] == bizType);
+
+                if (validatorType != null)
+                {
+                    ValidatorHub.TryAdd(bizType, validatorType);
+                }
+            }
+
+            if (validatorType != null)
+            {
+                var validator = (FluentValidation.IValidator)Activator.CreateInstance(validatorType);
+                var validateResult = validator.Validate(bizObj);
+                if (!validateResult.IsValid)
+                {
+                    return (false, validateResult.ToString());
+                }
+            }
+
+            return (true, null);
         }
     }
 }
