2745cbc3ed114b31d3d872e04d2871b0d35dab3f
mkc-xuliangjie <xuliangjie19870208@163.com>
2019-09-29 16:32:11 +0800

    code

---
+++ Src/BlueCat.Api/BuleCat.Common/Http/BaseModel/RequsetModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueCat.Contract
+{
+    public class RequsetModel
+    {
+        /// <summary>
+        /// 请求的应用名称
+        /// </summary>
+        [Required]
+        public string Application { get; set; }
+
+        /// <summary>
+        /// 请求的数据
+        /// </summary>
+        [Required]
+        public string Data { get; set; }
+
+        /// <summary>
+        /// 当前用户（若有验证）
+        /// </summary>
+        public string Account { get; set; }
+    }
+
+    public class RequsetModel<T> : RequsetModel where T : class, new()
+    {
+        /// <summary>
+        /// 业务数据，将 <see cref="RequsetModel.Data"/> 反序列化为指定的对象 <typeparamref name="T"/>, 若不能反序列化，将设置为 <code>default(T)</code>
+        /// </summary>
+        public T BusinessData { get; set; }
+    }
+}
---
+++ Src/BlueCat.Api/BuleCat.Common/Http/BaseModel/ResponseModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuleCat.Common
+{
+    /// <summary>
+    /// 基础返回结果(返回数据)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ResponseModel<T> : ResponseModel
+    {
+        /// <summary>
+        /// 返回数据
+        /// </summary>
+        public T ResultData { get; set; }
+
+        public static ResponseModel<T> Success(T data)
+        {
+            return new ResponseModel<T>
+            {
+                ResultData = data
+            };
+        }
+
+        public new static ResponseModel<T> Failure(string code, string desc)
+        {
+            return new ResponseModel<T>
+            {
+                ResultCode = code,
+                ResultDesc = desc
+            };
+        }
+    }
+
+
+    /// <summary>
+    /// 基础返回结果(不返回数据)
+    /// </summary>
+    public class ResponseModel
+    {
+        /// <summary>
+        /// 请求id，每个请求唯一
+        /// </summary>
+        public string RequestId { get; set; }
+
+        /// <summary>
+        /// 返回状态编码
+        /// </summary>
+        public string ResultCode { get; set; }
+
+        /// <summary>
+        /// 返回描述
+        /// </summary>
+        public string ResultDesc { get; set; }
+
+        /// <summary>
+        /// 服务器时间
+        /// </summary>
+        public long ServerTime { get; set; }
+
+        /// <summary>
+        /// 返回提示
+        /// </summary>
+        //public string Message { get; set; }
+
+        /// <summary>
+        /// 默认构造函数
+        /// </summary>
+        public ResponseModel()
+        {
+            RequestId = HttpContextGlobal.CurrentTraceId;
+            ResultCode = "0000";
+            ResultDesc = "success";
+            ServerTime = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+        }
+
+        public static ResponseModel Failure(string code, string desc)
+        {
+            return new ResponseModel
+            {
+                ResultCode = code,
+                ResultDesc = desc
+            };
+        }
+    }
+}
