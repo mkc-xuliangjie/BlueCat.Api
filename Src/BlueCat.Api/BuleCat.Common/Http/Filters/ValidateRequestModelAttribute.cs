b9670ad7a087e6c98be56ce9b02aebb746173d4c
mkc-xuliangjie <xuliangjie19870208@163.com>
2019-09-29 17:55:51 +0800

    code

---
+++ Src/BlueCat.Api/BuleCat.Common/Http/Filters/ValidateRequestModelAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuleCat.Common.Http.Filters
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ValidateRequestModelAttribute : ActionFilterAttribute
+    {
+        private static readonly ConcurrentDictionary<Type, Type> ValidatorHub = new ConcurrentDictionary<Type, Type>();
+
+        public virtual void ValidateCore(ActionExecutingContext context)
+        {
+
+        }
+    }
+}
